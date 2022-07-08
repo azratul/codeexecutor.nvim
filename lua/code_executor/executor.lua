da1b4925b09eeceba1820e649cb22361fd6ba174
Claudio Lepín <claudio.lepin@uchile.cl>
2022-07-07 21:25:43 -0400

    Close option was added

--- lua/code_executor/executor.lua
+++ lua/code_executor/executor.lua
@@ -4,6 +4,8 @@ local cmd = vim.cmd
 local fn = vim.fn
 local conf = {}
 
+BufferName = 10000
+
 function M.setup(config)
     conf = config
 end
@@ -15,13 +17,19 @@ end
 function M.run(file)
     local _, _, ext = splitFilename(file)
     if conf[ext] ~= nil then
-        pcall(cmd, 'split terminal')
+        pcall(cmd, 'split running-app')
         pcall(cmd, 'resize 10')
         fn.termopen('bash -c "' .. conf[ext]['command'] .. ' ' .. file .. '"')
+        pcall(cmd, 'normal G')
+        BufferName = vim.api.nvim_buf_get_name(0)
         print('Running...')
     else
         print('No command for this file type')
     end
+end
+
+function M.close()
+    pcall(cmd, 'bdelete! ' .. BufferName)
 end
 
 return M
--- plugin/azratul.vim
+++ plugin/azratul.vim
@@ -4,7 +4,8 @@ let s:save_cpo = &cpo " save user coptions
 set cpo&vim           " reset them to defaults
 
 " command to run our plugin
-command! -complete=file -nargs=1 CodeExecutor lua require("code_executor").run(<q-args>)
+command! -complete=file -nargs=1 CodeExecutorStart lua require("code_executor").run(<q-args>)
+command! CodeExecutorStop lua require("code_executor").close()
 
 let &cpo = s:save_cpo " and restore after
 unlet s:save_cpo
